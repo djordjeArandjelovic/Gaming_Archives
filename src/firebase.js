4cb85b6e20556511a376286a5c1d0b5d6d820091
Djordje Arandjelovic <djordjearandjelovic993@gmail.com>
2023-09-15 23:24:42 +0200

    initialize Firestore in firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
 	apiKey: "AIzaSyD81cC22Lq69xuZJfPw6_r4-yvWkUfbNQw",
@@ -13,3 +14,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+export const db = getFirestore(app);
