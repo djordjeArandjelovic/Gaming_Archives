9d032150f30b8b7a30808e8a5dbec6e3654fcb7e
Djordje Arandjelovic <djordjearandjelovic993@gmail.com>
2023-09-16 13:32:09 +0200

    Start of user profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,21 @@
+import { Box, Flex, Heading } from "@chakra-ui/react";
 import React from "react";
 import NavBar from "../components/NavBar";
+import { useAuth } from "../context/useAuth";
 
 const Profile = () => {
+	const { user } = useAuth();
 	return (
 		<>
 			<NavBar />
-			<div>Profile</div>
+			<Flex p={5}>
+				<Box>
+					<Flex flexDirection={"column"} gap={4}>
+						<Heading>User Profile:</Heading>
+						<Heading fontSize={"lg"}> email: {user?.email} </Heading>
+					</Flex>
+				</Box>
+			</Flex>
 		</>
 	);
 };
