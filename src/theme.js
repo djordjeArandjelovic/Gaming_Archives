082bdc6f57917879158db6a8e85fa6adcc517647
Djordje Arandjelovic <djordjearandjelovic993@gmail.com>
2023-10-19 22:47:30 +0200

    color switch on light more fix

--- src/theme.js
+++ src/theme.js
@@ -1,3 +1,29 @@
+// import { extendTheme } from "@chakra-ui/react";
+
+// const config = {
+// 	initialColorMode: "dark",
+// };
+
+// const theme = extendTheme({
+// 	config,
+// 	colors: {
+// 		gray: {
+// 			50: "#f9f9f9",
+// 			100: "#ededed",
+// 			200: "#d3d3d3",
+// 			300: "b3b3b3",
+// 			400: "#a0a0a0",
+// 			500: "#898989",
+// 			600: "6c6c6c",
+// 			700: "#202020",
+// 			800: "#121212",
+// 			900: "#111",
+// 		},
+// 	},
+// });
+
+// export default theme;
+
 import { extendTheme } from "@chakra-ui/react";
 
 const config = {
@@ -18,6 +44,20 @@ const theme = extendTheme({
 			700: "#202020",
 			800: "#121212",
 			900: "#111",
+		},
+	},
+	components: {
+		Switch: {
+			baseStyle: {
+				track: {
+					dark: {
+						bg: "purple.300",
+					},
+					_light: {
+						bg: "purple.300",
+					},
+				},
+			},
 		},
 	},
 });
